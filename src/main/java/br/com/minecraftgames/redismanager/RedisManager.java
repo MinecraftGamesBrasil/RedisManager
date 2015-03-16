d05a778faa9b7e831d3ad6950c8dbb52fcb1165d
RamonSaboya <ramons.gomes@hotmail.com>
2015-03-16 00:14:50 -0300

    Initial commit

---
+++ src/main/java/br/com/minecraftgames/redismanager/RedisManager.java
@@ -0,0 +1,14 @@
+package br.com.minecraftgames.redismanager;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class RedisManager extends Plugin {
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+}
