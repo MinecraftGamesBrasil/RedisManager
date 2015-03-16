7999065216d5b2ccb9bb48035e0056bcf7e74d12
RamonSaboya <ramons.gomes@hotmail.com>
2015-03-16 00:21:21 -0300

    Update RedisManager.java

--- src/main/java/br/com/minecraftgames/redismanager/RedisManager.java
+++ src/main/java/br/com/minecraftgames/redismanager/RedisManager.java
@@ -5,10 +5,10 @@ import net.md_5.bungee.api.plugin.Plugin;
 public class RedisManager extends Plugin {
 
     public void onEnable() {
-
+        
     }
 
     public void onDisable() {
-
+        
     }
 }
